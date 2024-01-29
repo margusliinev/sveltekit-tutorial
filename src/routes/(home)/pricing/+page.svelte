41cb59e586cd1a3eecc5508ecdab57161b258a5d
Margus Liinev <margus.liinev@gmail.com>
2024-01-29 04:42:08 +0200

    added default error page, pricing page and routing for home and app layouts

---
+++ src/routes/(home)/pricing/+page.svelte
@@ -0,0 +1,124 @@
+<section class="my-16">
+    <div class="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+        <div class="mx-auto mb-8 max-w-screen-md text-center lg:mb-12">
+            <h1 class="mb-4 text-5xl font-bold tracking-tight">Pricing</h1>
+            <h2 class="mb-4 text-3xl font-semibold tracking-tight">Designed for business teams like yours</h2>
+            <p class="mb-5 font-light text-white/80 sm:text-xl">
+                We have you covered, no matter the size of your team. Unsure which plan is right for you? Start with the plan that suits your needs today, and upgrade later.
+            </p>
+        </div>
+        <div class="space-y-8 sm:gap-6 lg:grid lg:grid-cols-3 lg:space-y-0 xl:gap-10">
+            <div class="mx-auto flex max-w-lg flex-col rounded-lg border border-zinc-600 bg-zinc-800 p-6 text-center shadow-lg xl:p-8">
+                <h3 class="mb-4 text-2xl font-semibold">Hobby</h3>
+                <p class="font-light text-white/80 sm:text-lg">Best option for personal use & for your next project.</p>
+                <div class="my-8 flex items-baseline justify-center">
+                    <span class="mr-2 text-5xl font-semibold">$0</span>
+                    <span>/month</span>
+                </div>
+                <ul class="mb-8 space-y-4 text-left">
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>Community support</span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>Up to 10 project boards</span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>
+                            Team size: <span class="font-semibold">1 developer</span>
+                        </span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>
+                            Premium support: <span class="font-semibold">6 months</span>
+                        </span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>
+                            Free updates: <span class="font-semibold">6 months</span>
+                        </span>
+                    </li>
+                </ul>
+                <a href="/register" class="rounded-full bg-zinc-200 px-5 py-2.5 text-center text-sm font-semibold text-primary-foreground transition-colors hover:bg-zinc-100"> Sign up for free </a>
+            </div>
+            <div class="mx-auto flex max-w-lg flex-col rounded-lg border border-zinc-600 bg-zinc-800 p-6 text-center shadow-lg xl:p-8">
+                <h3 class="mb-4 text-2xl font-semibold">Startup</h3>
+                <p class="font-light text-white/80 sm:text-lg">Relevant for small agile teams with extended support</p>
+                <div class="my-8 flex items-baseline justify-center">
+                    <span class="mr-2 text-5xl font-semibold">$99</span>
+                    <span>/month</span>
+                </div>
+                <ul class="mb-8 space-y-4 text-left">
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>Business hour support</span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>Unlimited project boards</span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>
+                            Team size: <span class="font-semibold">12 developers</span>
+                        </span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>
+                            Premium support: <span class="font-semibold">24 months</span>
+                        </span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>
+                            Free updates: <span class="font-semibold">24 months</span>
+                        </span>
+                    </li>
+                </ul>
+                <a href="/register" class="rounded-full bg-primary px-5 py-2.5 text-center text-sm font-semibold text-primary-foreground transition-colors hover:bg-primary-hover"> Get started </a>
+            </div>
+            <div class="mx-auto flex max-w-lg flex-col rounded-lg border border-zinc-600 bg-zinc-800 p-6 text-center shadow-lg xl:p-8">
+                <h3 class="mb-4 text-2xl font-semibold">Enterprise</h3>
+                <p class="font-light text-white/80 sm:text-lg">Best for large teams with high usage and need for premium support.</p>
+                <div class="my-8 flex items-baseline justify-center">
+                    <span class="mr-2 text-5xl font-semibold">$799</span>
+                    <span>/month</span>
+                </div>
+                <ul class="mb-8 space-y-4 text-left">
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>24/7 Priority support</span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>Unlimited project boards</span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>
+                            Team size: <span class="font-semibold">100+ developers</span>
+                        </span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>
+                            Premium support: <span class="font-semibold">36 months</span>
+                        </span>
+                    </li>
+                    <li class="flex items-center space-x-3">
+                        <span class="text-emerald-400">&#10004;</span>
+                        <span>
+                            Free updates: <span class="font-semibold">36 months</span>
+                        </span>
+                    </li>
+                </ul>
+                <a href="/register" class="rounded-full bg-yellow-200 px-5 py-2.5 text-center text-sm font-semibold text-primary-foreground transition-colors hover:bg-yellow-100"> Available soon </a>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<main class="grid h-full min-h-screen w-full place-items-center">
+    <div class="w-screen-90 text-center">
+        <p class="text-7xl font-bold text-primary">{$page.status}</p>
+        <h1 class="mt-4 text-4xl font-semibold tracking-tight xxs:text-5xl">
+            {#if $page.status === 404}
+                Page Not Found
+            {:else}
+                {$page.error?.message || 'Internal Server Error'}
+            {/if}
+        </h1>
+        <p class="text-foreground/90 mt-6 text-lg">
+            {#if $page.status === 404}
+                Sorry, the page you are looking for could not be found.
+            {:else}
+                We encountered an error and cannot fulfill the request.
+            {/if}
+        </p>
+        <div class="mt-8 flex items-center justify-center gap-6 text-sm font-semibold">
+            <a href="/" class="rounded-full bg-primary px-3.5 py-2.5 text-primary-foreground transition-colors hover:bg-primary-hover">Go Back Home</a>
+            <a href="/login" class="group flex items-center gap-1">
+                Login Page
+                <span aria-hidden="true" class="font-normal transition-colors group-hover:text-primary">&rarr;</span>
+            </a>
+        </div>
+    </div>
+</main>
